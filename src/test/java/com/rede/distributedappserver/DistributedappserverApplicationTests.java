0b9e83d47b38b50a66799b332f7f5a2cfb7357f4
Shiva Ram Reddy Bethi <iamreddy005@gmail.com>
2024-09-13 13:38:03 -0700

    Initial commit

---
+++ src/test/java/com/rede/distributedappserver/DistributedappserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rede.distributedappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedappserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
